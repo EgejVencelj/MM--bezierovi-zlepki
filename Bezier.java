f4dcadc708b77fea64872a607fa15f0bb299f1c5
d3jus <jus@debelak.net>
2015-06-10 17:32:14 +0200

    Dodano gladko lepljene krivulj

--- Bezier.java
+++ Bezier.java
@@ -17,9 +17,9 @@ public class Bezier {
 	}
 	
 	public Bezier() {
-		JFrame f = new JFrame();
+		JFrame f = new JFrame("Bezierovi zlepki");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(200, 200);
+		f.setSize(600, 480);
 		
 		Pane p = new Pane(); 
 		
@@ -36,7 +36,6 @@ class Pane extends JPanel {
 	private double x;
 	private double y;
 	private List<Point2D> points = new ArrayList<Point2D>();
-	//private List<Ellipse2D> paintPoints = new ArrayList<Point2D>();
 	private List<Ellipse2D> paintControlPoints = new ArrayList<Ellipse2D>();
 	private List<CubicCurve2D> curves = new ArrayList<CubicCurve2D>();
 	private int mouseDownOn = -1;
@@ -63,18 +62,24 @@ class Pane extends JPanel {
 				points.add(new Point2D.Double(x, y));
 				
 				int pointsSize = points.size();
-			
+				int curvesSize = curves.size();
+				
 				if(pointsSize > 1) {
-					Point2D prev = points.get(pointsSize - 2);
-					System.out.println(x + " " + y + " - " + prev.getX() + " " + prev.getY());
+					Point2D prevPoint = points.get(pointsSize - 2);
+					CubicCurve2D prevCurve = null;
 					
-					// Dodamo novo krivuljo (tole je samo zacasno)
-					double ctrlX1 = x - 10;
-					double ctrlY1 = y - 10;
-					double ctrlX2 = prev.getX() - 10;
-					double ctrlY2 = prev.getY() - 10;
+					if(curvesSize > 0)
+						prevCurve = curves.get(curvesSize - 1);
 					
-					CubicCurve2D c = new CubicCurve2D.Double(x, y, ctrlX1, ctrlY1, ctrlX2, ctrlY2, prev.getX(), prev.getY());
+					//System.out.println(x + " " + y + " - " + prevPoint.getX() + " " + prevPoint.getY());
+					
+					// Dodamo novo krivuljo https://www.particleincell.com/2012/bezier-splines/
+					double ctrlX2 = (x + prevPoint.getX()) * 0.5;//r.nextInt(30) + 5;
+					double ctrlY2 = (y + prevPoint.getY()) * 0.5;
+					double ctrlX1 = (curvesSize > 0 ? (2 * prevCurve.getX2() - prevCurve.getCtrlX2()) : prevPoint.getX());
+					double ctrlY1 = (curvesSize > 0 ? (2 * prevCurve.getY2() - prevCurve.getCtrlY2()) : prevPoint.getY() + 30);
+					
+					CubicCurve2D c = new CubicCurve2D.Double(prevPoint.getX(), prevPoint.getY(), ctrlX1, ctrlY1, ctrlX2, ctrlY2, x, y);
 					curves.add(c);
 					
 					// Dodamo obe kontrolni tocki za slikanje
@@ -89,22 +94,82 @@ class Pane extends JPanel {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				mouseDownOn = -1;
-				System.out.println(e.getX());
 			}
 		});
 		this.addMouseMotionListener(new MouseAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				if(mouseDownOn > -1) {
-					CubicCurve2D temp = curves.get(mouseDownOn / 2);
+				if(mouseDownOn > -1) {	
+					// i nam pove ali spreminjamo prvo kontrolno tocko ali drugo
 					int i = mouseDownOn % 2;
-					double ctrlX1 = (i == 0 ? e.getX() : temp.getCtrlX1());
-					double ctrlY1 = (i == 0 ? e.getY() : temp.getCtrlY1());
-					double ctrlX2 = (i == 1 ? e.getX() : temp.getCtrlX2());
-					double ctrlY2 = (i == 1 ? e.getY() : temp.getCtrlY2());
+					CubicCurve2D temp = curves.get(mouseDownOn / 2);
+					CubicCurve2D temp1 = null;
 					
-					paintControlPoints.set(mouseDownOn, new Ellipse2D.Double(e.getX() - (POINTSIZE / 2), e.getY() - (POINTSIZE / 2), POINTSIZE, POINTSIZE));
-					temp.setCurve(temp.getX1(), temp.getY1(), ctrlX1, ctrlY1, ctrlX2, ctrlY2, temp.getX2(), temp.getY2());
+					if((mouseDownOn / 2 - 1 > 0) && (mouseDownOn / 2 + 1) < curves.size()) {
+						temp1 = curves.get(mouseDownOn / 2 + (i == 0 ? -1 : 1));
+					}
+
+					// Ce popravljamo kontrolno tocko, moramo popraviti tudi sosede
+					if(temp1 != null) {
+						// Popravljamo 2. kontrolno pri prejsnji krivulji
+						if(i == 0) {
+							paintControlPoints.set(mouseDownOn - 1, 
+								new Ellipse2D.Double(
+									(2 * temp1.getX2() - e.getX()) - (POINTSIZE / 2), 
+									(2 * temp1.getY2() - e.getY()) - (POINTSIZE / 2), 
+									POINTSIZE, 
+									POINTSIZE)
+							);
+							temp1.setCurve(
+								temp1.getX1(), 
+								temp1.getY1(), 
+								temp1.getCtrlX1(), 
+								temp1.getCtrlY1(), 
+								(2 * temp1.getX2() - e.getX()), 
+								(2 * temp1.getY2() - e.getY()), 
+								temp1.getX2(), 
+								temp1.getY2()
+							);
+						} else {
+							// Popravljamo 1. kontrolno pri naslednji krivulji
+							paintControlPoints.set(mouseDownOn + 1, 
+								new Ellipse2D.Double(
+									(2 * temp1.getX1() - e.getX()) - (POINTSIZE / 2), 
+									(2 * temp1.getY1() - e.getY()) - (POINTSIZE / 2), 
+									POINTSIZE, 
+									POINTSIZE)
+							);
+							temp1.setCurve(
+								temp1.getX1(), 
+								temp1.getY1(), 
+								(2 * temp1.getX1() - e.getX()), 
+								(2 * temp1.getY1() - e.getY()), 
+								temp1.getCtrlX2(), 
+								temp1.getCtrlY2(), 
+								temp1.getX2(), 
+								temp1.getY2()
+							);
+						}
+					}
+					
+					// Izbrana tocka
+					paintControlPoints.set(mouseDownOn,
+						new Ellipse2D.Double(
+							e.getX() - (POINTSIZE / 2), 
+							e.getY() - (POINTSIZE / 2), 
+							POINTSIZE, 
+							POINTSIZE)
+					);
+					temp.setCurve(
+						temp.getX1(), 
+						temp.getY1(), 
+						(i == 0 ? e.getX() : temp.getCtrlX1()), 
+						(i == 0 ? e.getY() : temp.getCtrlY1()), 
+						(i == 1 ? e.getX() : temp.getCtrlX2()), 
+						(i == 1 ? e.getY() : temp.getCtrlY2()), 
+						temp.getX2(), 
+						temp.getY2()
+					);
 					
 					repaint();
 				}
@@ -121,7 +186,7 @@ class Pane extends JPanel {
 		
 		g2.setColor(Color.blue);
 		for(Point2D p: points) {
-			g2.draw(new Ellipse2D.Double(p.getX() - 4, p.getY() - 4, 8, 8));
+			g2.draw(new Ellipse2D.Double(p.getX() - (POINTSIZE / 2), p.getY() - (POINTSIZE / 2), POINTSIZE, POINTSIZE));
 		}
 			
 		for(CubicCurve2D c: curves) {
@@ -138,5 +203,6 @@ class Pane extends JPanel {
 			g2.draw(e);
 		}
 	}
-
 }
+
+         
